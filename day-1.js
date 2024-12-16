b83149b98539d7c9234713b5f50703be5532bc1c
miccou <30512204+miccou@users.noreply.github.com>
2024-12-16 20:44:45 +1100

    day 1 puzzle 2

--- day-1.js
+++ day-1.js
@@ -1,54 +1,64 @@
 // run me with:
 // node day-1.js
 
-let fs = require('fs'),
-    path = require('path'),
-    filePath = path.join(__dirname, 'puzzle-inputs/day-1-1.txt');
+let fs = require("fs");
+path = require("path");
+// filePath = path.join(__dirname, "puzzle-inputs/day-1-example.txt");
+filePath = path.join(__dirname, "puzzle-inputs/day-1.txt");
 
 const quickSort = (arr) => {
-    if (arr.length <= 1) {
-        return arr;
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  let pivot = arr[0];
+  let leftArr = [];
+  let rightArr = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      leftArr.push(arr[i]);
+    } else {
+      rightArr.push(arr[i]);
     }
+  }
 
-    let pivot = arr[0];
-    let leftArr = [];
-    let rightArr = [];
-
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] < pivot) {
-            leftArr.push(arr[i]);
-        } else {
-            rightArr.push(arr[i]);
-        }
-    }
-
-    return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
+  return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
 };
 
 let col0 = [];
 let col1 = [];
-let differences = []
+let differences = []; //part 1
+let similarityScores = []; //part 2
 
-fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
-    if (!err) {
-        data.split('\n').map((l) => {
-            let data = l.replaceAll(" ", " ").trim().split(" ").filter((x) => !!x || x.trim() != '');
-            col0.push(data[0]);
-            col1.push(data[1]);
-        });
+fs.readFile(filePath, { encoding: "utf-8" }, function (err, data) {
+  if (!err) {
+    data.split("\n").map((l) => {
+      let data = l
+        .replaceAll(" ", " ")
+        .trim()
+        .split(" ")
+        .filter((x) => !!x || x.trim() != "");
+      col0.push(data[0]);
+      col1.push(data[1]);
+    });
 
-        col0 = quickSort(col0);
-        col1 = quickSort(col1);
+    col0.forEach((element, index) => {
+      let matches = col1.filter((x) => x == element).length;
+      similarityScores.push(matches * element);
+    });
 
-        col0.forEach((element, index) => {
-            let diff = Math.abs(element - col1[index]);
-            differences.push(diff);
-        });
+    col0 = quickSort(col0);
+    col1 = quickSort(col1);
 
-        console.log(differences.reduce((a, b) => a + b))
+    col0.forEach((element, index) => {
+      let diff = Math.abs(element - col1[index]);
+      differences.push(diff);
+    });
 
-
-    } else {
-        console.log(err);
-    }
+    console.log(differences.reduce((a, b) => a + b)); //part1
+    console.log(similarityScores.reduce((a, b) => a + b)); //part 2
+  } else {
+    console.log(err);
+  }
 });
